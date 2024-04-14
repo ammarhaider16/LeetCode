fcd8f1fa6f35573140d18664cd149826338fdb06
Ammar Haider <148816794+ammarhaider16@users.noreply.github.com>
2024-04-14 03:57:08 -0400

    Time: 25 ms (8.13%), Space: 45.6 MB (5.3%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,20 +1,20 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
+
         if (s.length() != t.length())
             return false;
 
-        Map<Character, Integer> letters = new HashMap<Character, Integer>();
+        Map<Character, ArrayList<Integer>> letters = new HashMap<Character, ArrayList<Integer>>();
         for (int i = 0; i < s.length(); i++) {
             if ((!letters.containsKey(s.charAt(i))))
-                letters.put((Character) s.charAt(i), 1);
+                letters.put((Character) s.charAt(i), new ArrayList<Integer>(Arrays.asList(i)));
             else
-                letters.put(s.charAt(i), letters.get((s.charAt(i))) + 1);
+                (letters.get(s.charAt(i))).add(i);
         }
 
         for (int i = 0; i < t.length(); i++) {
-            System.out.println(letters);
-            if (letters.get(t.charAt(i)) != null && (int) letters.get(t.charAt(i)) > 0)
-                letters.put(t.charAt(i), (int) letters.get((t.charAt(i))) - 1);
+            if (letters.get(t.charAt(i)) != null && letters.get(t.charAt(i)).size() > 0)
+                letters.get(t.charAt(i)).remove(0);
             else
                 return false;
         }
