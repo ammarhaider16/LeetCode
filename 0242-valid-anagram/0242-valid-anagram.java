283739287646f30b4e924a46757d8d7507ba651a
Ammar Haider <148816794+ammarhaider16@users.noreply.github.com>
2024-04-14 03:37:28 -0400

    Time: 23 ms (9.86%), Space: 45.4 MB (5.3%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length())
+            return false;
+
+        Map<Character, ArrayList<Integer>> letters = new HashMap<Character, ArrayList<Integer>>();
+        for (int i = 0; i < s.length(); i++) {
+            if ((!letters.containsKey(s.charAt(i))))
+                letters.put((Character) s.charAt(i), new ArrayList<Integer>(Arrays.asList(i)));
+            else
+                (letters.get(s.charAt(i))).add(i);
+        }
+
+        for (int i = 0; i < t.length(); i++) {
+            if (letters.get(t.charAt(i))!=null && letters.get(t.charAt(i)).size()>0)
+                letters.get(t.charAt(i)).remove(0);
+            else
+                return false;
+        }
+        return true;
+    }
+}
