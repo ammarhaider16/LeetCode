d1aaa05d8c1999a943e5a585beb04a8cca4b4b5f
Ammar Haider <148816794+ammarhaider16@users.noreply.github.com>
2024-06-07 02:25:07 -0400

    Time: 15 ms (44.52%), Space: 47.6 MB (31.97%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.java
+++ 0542-01-matrix/0542-01-matrix.java
@@ -1,37 +1,57 @@
 class Solution {
     
-    public int[][] updateMatrix(int[][] mat) {
+        public int[][] updateMatrix(int[][] mat) {
+        Queue<int[]> zeros = new LinkedList<int[]>();
         int m = mat.length;
         int n = mat[0].length;
-        int[][] posMat = new int[m][n];
-
+        int highVal = m * n;
+        // Step 1: Initially adding things to the queue
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
-                if (mat[i][j] == 1) posMat[i][j] = zeroPos(i, j, mat);
+                if (mat[i][j] == 1)
+                    mat[i][j] = highVal;
+                else {
+                    int[] idxs = {i, j};
+                    zeros.add(idxs);
+                } 
+            } 
+        }
+
+        // Step 2: BFS Traversal 
+        while (!zeros.isEmpty()) {
+            int[] idxs = zeros.poll();
+            int row = idxs[0];
+            int col = idxs[1];
+            
+            // check left neighbour
+            if (row > 0 && mat[row][col] + 1 < mat[row-1][col]) {
+                mat[row-1][col] = mat[row][col] + 1;
+                int[] toQ = {row-1,col};
+                zeros.add(toQ);
+            }
+            // check right neighbour
+            if (row < m - 1 && mat[row][col] + 1 < mat[row+1][col]) {
+                mat[row+1][col] = mat[row][col] + 1;
+                int[] toQ = {row+1,col};
+                zeros.add(toQ);
+            }
+
+            // check top neighbour
+            if (col > 0 && mat[row][col] + 1 < mat[row][col-1]) {
+                mat[row][col-1] = mat[row][col] + 1;
+                int[] toQ = {row,col-1};
+                zeros.add(toQ);
+            }
+
+            // check bottom neighbour
+            if (col < n - 1 && mat[row][col] + 1 < mat[row][col+1]) {
+                mat[row][col+1] = mat[row][col] + 1;
+                int[] toQ = {row,col+1};
+                zeros.add(toQ);
             }
         }
 
-        return posMat;
-    }
-
-    public int zeroPos(int row, int col, int[][] mat) {
-
-        int m = mat.length;
-        int n = mat[0].length;
-
-        int lowestDistance = Integer.MAX_VALUE - 1000;
-
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (mat[i][j] == 0) {
-                    int thisDist = Math.abs(row - i) + Math.abs(col - j);
-                    if (thisDist < lowestDistance)
-                        lowestDistance = thisDist;
-                }
-            }
-        }
-
-        return lowestDistance;
+        return mat;
     }
 
 }
