a6ed510c48943f5b35194aaf93051629de8a018e
Ammar Haider <148816794+ammarhaider16@users.noreply.github.com>
2024-07-25 01:29:28 -0400

    Time: 1 ms (99.96%), Space: 55.6 MB (40.54%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.java
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -1,25 +1,25 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
-        HashMap<Integer, Integer> prefixMultiples = new HashMap<>();
-        HashMap<Integer, Integer> suffixMultiples = new HashMap<>();
         int len = nums.length;
+        int[] prefixMultiples = new int[len];
+        int[] suffixMultiples = new int[len];
 
-        prefixMultiples.put(0, nums[0]);
-        suffixMultiples.put(len - 1, nums[len - 1]);
+        prefixMultiples[0] = nums[0];
+        suffixMultiples[len - 1] = nums[len - 1];
         for (int i = 1; i < len; i++) {
-            prefixMultiples.put(i, nums[i] * prefixMultiples.get(i - 1));
+            prefixMultiples[i] = nums[i] * prefixMultiples[i - 1];
         }
 
         for (int j = len - 2; j >= 0; j--) {
-            suffixMultiples.put(j, nums[j] * suffixMultiples.get(j + 1));
+            suffixMultiples[j] = nums[j] * suffixMultiples[j + 1];
         }
 
         int[] products = new int[len];
 
-        products[0] = suffixMultiples.get(1);
-        products[len - 1] = prefixMultiples.get(len - 2);
+        products[0] = suffixMultiples[1];
+        products[len - 1] = prefixMultiples[len - 2];
         for (int k = 1; k < len - 1; k++) {
-            products[k] = prefixMultiples.get(k - 1) * suffixMultiples.get(k + 1);
+            products[k] = prefixMultiples[k - 1] * suffixMultiples[k + 1];
         }
         return products;
     }
