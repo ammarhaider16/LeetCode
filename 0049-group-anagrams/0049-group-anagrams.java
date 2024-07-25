a0027c701de5c4b8f945f1c145c7a56a6c735398
Ammar Haider <148816794+ammarhaider16@users.noreply.github.com>
2024-07-25 00:02:47 -0400

    Time: 10 ms (28.83%), Space: 47.2 MB (97.72%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -1,30 +1,57 @@
-class Solution {
-    public List<List<String>> groupAnagrams(String[] strs) {
-        HashMap<HashMap<Character, Integer>, List<String>> groups = new HashMap<>();
+import java.util.*;
 
+public class Solution {
+
+    public List<List<String>> groupAnagrams(String[] strs) {
+        HashMap<String, List<String>> groups = new HashMap<>();
         for (String str : strs) {
-            HashMap<Character, Integer> strChars = new HashMap<>();
-            for (Character c : str.toCharArray()) {
-                Integer count = strChars.get(c);
-                if (count != null) {
-                    strChars.put(c, count + 1);
-                } else {
-                    strChars.put(c, 1);
-                }
-            }
-            List<String> anagrams = groups.get(strChars);
+            char[] charArray = str.toCharArray();
+            Arrays.sort(charArray);
+            String sorted = new String(charArray);
+            List<String> anagrams = groups.get(sorted);
             if (anagrams != null) {
                 anagrams.add(str);
             } else {
                 List<String> newList = new ArrayList<>(Arrays.asList(str));
-                groups.put(strChars, newList);
+                groups.put(sorted, newList);
             }
         }
-
         List<List<String>> finalList = new ArrayList<>();
-        for (HashMap<Character, Integer> anagramMap : groups.keySet()) {
-            finalList.add(groups.get(anagramMap));
+        for (String group : groups.keySet()) {
+            finalList.add(groups.get(group));
         }
         return finalList;
     }
+
+    /*
+     * public List<List<String>> groupAnagrams(String[] strs) {
+     * HashMap<HashMap<Character, Integer>, List<String>> groups = new HashMap<>();
+     * 
+     * for (String str : strs) {
+     * HashMap<Character, Integer> strChars = new HashMap<>();
+     * for (Character c : str.toCharArray()) {
+     * Integer count = strChars.get(c);
+     * if (count != null) {
+     * strChars.put(c, count + 1);
+     * } else {
+     * strChars.put(c, 1);
+     * }
+     * }
+     * List<String> anagrams = groups.get(strChars);
+     * if (anagrams != null) {
+     * anagrams.add(str);
+     * } else {
+     * List<String> newList = new ArrayList<>(Arrays.asList(str));
+     * groups.put(strChars, newList);
+     * }
+     * }
+     * 
+     * List<List<String>> finalList = new ArrayList<>();
+     * for (HashMap<Character, Integer> anagramMap : groups.keySet()) {
+     * finalList.add(groups.get(anagramMap));
+     * }
+     * return finalList;
+     * }
+     */
+
 }
