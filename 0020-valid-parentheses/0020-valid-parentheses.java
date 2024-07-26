ae72fe694b4368b7aa9b7c7d88ccd8de741a1483
Ammar Haider <148816794+ammarhaider16@users.noreply.github.com>
2024-07-26 17:01:53 -0400

    Time: 1 ms (98.92%), Space: 41.5 MB (42.7%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,35 +1,25 @@
 class Solution {
     public boolean isValid(String s) {
-
-        Stack braks = new Stack<Character>();
-        System.out.println(braks.size());
-
+        Stack<Character> brackets = new Stack<>();
         for (int i = 0; i < s.length(); i++) {
-            if (braks.size() == 0) {
-                braks.push(s.charAt(i));
-                continue;
-            }
-
-            Character reversed;
-            if (s.charAt(i) == ')')
-                reversed = '(';
-            else if (s.charAt(i) == ']')
-                reversed = '[';
-            else
-                reversed = '{';
-
-            if ((s.charAt(i) == ')' || s.charAt(i) == ']' || s.charAt(i) == '}') && (char) braks.peek() != reversed)
+            char c = s.charAt(i);
+            if (c == '(' || c == '{' || c == '[')
+                brackets.push(c);
+            else if (brackets.isEmpty())
                 return false;
-            else if (s.charAt(i) == ')' || s.charAt(i) == ']' || s.charAt(i) == '}') {
-                braks.pop();
-            } else
-                braks.push(s.charAt(i));
-
+            else if (c == ']') {
+                if (brackets.pop() != '[')
+                    return false;
+            } else if (c == '}') {
+                if (brackets.pop() != '{')
+                    return false;
+            } else if (c == ')') {
+                if (brackets.pop() != '(')
+                    return false;
+            }
         }
-
-        if (braks.size() != 0)
+        if (!brackets.isEmpty())
             return false;
-
         return true;
-    }    
+    }
 }
