b9812d879cecef9a2a0e136ecca4137fd34ca547
Ammar Haider <148816794+ammarhaider16@users.noreply.github.com>
2024-07-25 23:36:36 -0400

    Time: 14 ms (34.87%), Space: 44.9 MB (28.03%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -2,24 +2,14 @@ class Solution {
 
     public boolean isPalindrome(String s) {
 
-        ArrayList<Character> alphaNumChars = new ArrayList<>();
-        String lowerStr = s.toLowerCase();
-
-        for (int i = 0; i < s.length(); i++) {
-            char c = lowerStr.charAt(i);
-            if ((c >= 48 && c <= 57) || (c >= 97 && c <= 122))
-                alphaNumChars.add(c);
-        }
+        char[] alphaNumChars = s.replaceAll("[^a-zA-Z0-9]", "").toLowerCase().toCharArray();
 
         int start = 0;
-        int end = alphaNumChars.size() - 1;
-
-        System.out.println(alphaNumChars);
+        int end = alphaNumChars.length - 1;
 
         while (start < end) {
-            if (alphaNumChars.get(start) != alphaNumChars.get(end))
+            if (alphaNumChars[start] != alphaNumChars[end])
                 return false;
-                
             start++;
             end--;
         }
