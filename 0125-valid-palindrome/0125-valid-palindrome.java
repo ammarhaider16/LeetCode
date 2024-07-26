20f032782989bdc6abdb2f0039d2569101a08da9
Ammar Haider <148816794+ammarhaider16@users.noreply.github.com>
2024-07-25 23:32:50 -0400

    Time: 13 ms (40.47%), Space: 46 MB (7.53%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,27 +1,28 @@
 class Solution {
 
     public boolean isPalindrome(String s) {
-        
-        // Two-Pointer Approach
-        int start = 0;
-        int end = s.length() - 1;
 
-        for (start = 0; start<end; start++, end--) {
+        ArrayList<Character> alphaNumChars = new ArrayList<>();
+        String lowerStr = s.toLowerCase();
 
-            if (!((s.charAt(start) >= '0' && s.charAt(start) <= '9') || (s.charAt(start) >= 'a' && s.charAt(start) <= 'z') || (s.charAt(start) >= 'A' && s.charAt(start) <= 'Z'))) {
-                end++;
-                continue;
-            }
-
-            if (!((s.charAt(end) >= '0' && s.charAt(end) <= '9') || (s.charAt(end) >= 'a' && s.charAt(end) <= 'z') || (s.charAt(end) >= 'A' && s.charAt(end) <= 'Z'))) {
-                start--;
-                continue;
-            }
-
-            if (!((""+s.charAt(start)).toLowerCase().equals((""+s.charAt(end)).toLowerCase()))) return false;
-
+        for (int i = 0; i < s.length(); i++) {
+            char c = lowerStr.charAt(i);
+            if ((c >= 48 && c <= 57) || (c >= 97 && c <= 122))
+                alphaNumChars.add(c);
         }
-        
+
+        int start = 0;
+        int end = alphaNumChars.size() - 1;
+
+        System.out.println(alphaNumChars);
+
+        while (start < end) {
+            if (alphaNumChars.get(start) != alphaNumChars.get(end))
+                return false;
+                
+            start++;
+            end--;
+        }
         return true;
     }
 }
