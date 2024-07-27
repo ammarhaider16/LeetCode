984be3e259370e0d11ccc8384db5dc98d2e3d0b0
Ammar Haider <148816794+ammarhaider16@users.noreply.github.com>
2024-07-27 14:51:06 -0400

    Time: 13 ms (78.6%), Space: 46.3 MB (32.05%) - LeetHub

---
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int[] nextGreaterElements(int[] nums) {
+        int len = nums.length;
+        int[] ans = new int[len];
+        for (int i = 0; i < len; i++) {
+            ans[i] = -1;
+        }
+        Stack<int[]> nge = new Stack<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            int num = nums[i];
+            while (!nge.isEmpty() && num > nge.peek()[0]) {
+                nge.pop();
+            }
+            int[] addNumIdx = { num, i };
+            nge.push(addNumIdx);
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            int num = nums[i];
+            while (!nge.isEmpty() && num > nge.peek()[0]) {
+                int[] val = nge.pop();
+                int valIdx = val[1];
+                ans[valIdx] = num;
+            }
+            int[] addNumIdx = { num, i };
+            nge.push(addNumIdx);
+        }
+        return ans;
+    }
+}
