e026678d4a95aad12027076e5b51d861ccc7ed9d
Ammar Haider <148816794+ammarhaider16@users.noreply.github.com>
2024-07-26 20:00:06 -0400

    Time: 4 ms (95.48%), Space: 44.6 MB (62.81%) - LeetHub

---
+++ 0155-min-stack/0155-min-stack.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+class MinStack {
+
+    private ArrayList<Integer> stackList;
+    private ArrayList<Integer> minList;
+    private int len;
+
+    public MinStack() {
+        stackList = new ArrayList<Integer>();
+        minList = new ArrayList<Integer>();
+        len = 0;
+    }
+
+    public void push(int val) {
+        stackList.add(val);
+        len++;
+        if (minList.size() == 0 || val <= minList.get(minList.size() - 1)) {
+            minList.add(val);
+        }
+    }
+
+    public void pop() {
+        int val = stackList.remove(len - 1);
+        len--;
+        if (val == minList.get(minList.size() - 1)) {
+            minList.remove(minList.size() - 1);
+        }
+    }
+
+    public int top() {
+        return stackList.get(len - 1);
+    }
+
+    public int getMin() {
+        return minList.get(minList.size() - 1);
+    }
+}
