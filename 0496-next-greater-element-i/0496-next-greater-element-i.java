5650f9578e86e414bd36282b4eaa9fb032d9cafc
Ammar Haider <148816794+ammarhaider16@users.noreply.github.com>
2024-07-27 00:33:02 -0400

    Time: 3 ms (91.71%), Space: 44.6 MB (5.89%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.java
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -1,20 +1,23 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
         int[] ans = new int[nums1.length];
-        HashMap<Integer, Integer> numToNGE = new HashMap<>();
-        Stack<Integer> ngeStack = new Stack<>();
+        HashMap<Integer, Integer> numToIdx = new HashMap<>();
+        Stack<int[]> ngeStack = new Stack<>();
+        int[] elemNGE = new int[nums2.length];
         for (int currIdx = 0; currIdx < nums2.length; currIdx++) {
             int num = nums2[currIdx];
-            numToNGE.put(num, -1);
-            while (!ngeStack.isEmpty() && num > ngeStack.peek()) {
-                int elem = ngeStack.pop();
-                numToNGE.put(elem, num);
+            elemNGE[currIdx] = -1;
+            while (!ngeStack.isEmpty() && num > ngeStack.peek()[0]) {
+                int elemIdx = ngeStack.pop()[1];
+                elemNGE[elemIdx] = num;
             }
-            ngeStack.push(num);
+            int[] numIdx = { num, currIdx };
+            ngeStack.push(numIdx);
+            numToIdx.put(num, currIdx);
         }
 
         for (int i = 0; i < nums1.length; i++) {
-            ans[i] = numToNGE.get(nums1[i]);
+            ans[i] = elemNGE[numToIdx.get(nums1[i])];
         }
 
         return ans;
