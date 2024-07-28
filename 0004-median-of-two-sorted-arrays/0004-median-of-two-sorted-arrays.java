fc60265245ce25bf0d182bffdcd2e7eb353468bd
Ammar Haider <148816794+ammarhaider16@users.noreply.github.com>
2024-07-28 18:39:26 -0400

    Time: 3 ms (41.49%), Space: 45.9 MB (44.32%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
@@ -1,50 +1,39 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-
-        int[] medianArr = new int[nums1.length+nums2.length];
-
-        int nums1Idx = 0;
-        int nums2Idx = 0;
-
-        for (int i = 0; i<medianArr.length;i++) {
-
-            if (nums1Idx < nums1.length && nums2Idx < nums2.length) {
-                if (nums1[nums1Idx] < nums2[nums2Idx]) {
-                    medianArr[i] = nums1[nums1Idx];
-                    nums1Idx ++;
-                }
-
-                else if (nums1[nums1Idx] == nums2[nums2Idx]) {
-                    medianArr[i] = nums1[nums1Idx];
-                    medianArr[i+1] = nums2[nums2Idx];
-                    nums1Idx++;
-                    nums2Idx++;
-                    i++;
-                }
-
-                else if (nums1[nums1Idx] > nums2[nums2Idx]) {
-                    medianArr[i] = nums2[nums2Idx];
-                    nums2Idx++;
-                }
-
-            }
-
-            else if (nums1Idx < nums1.length) {
-                medianArr[i] = nums1[nums1Idx];
-                nums1Idx ++;
-            }
-
-            else if (nums2Idx < nums2.length) {
-                medianArr[i] = nums2[nums2Idx];
-                nums2Idx++;
+        int l1 = nums1.length;
+        int l2 = nums2.length;
+        ArrayList<Integer> srtd = new ArrayList<>();
+        int a = 0;
+        int b = 0;
+        while (a < l1 && b < l2) {
+            if (nums1[a] < nums2[b]) {
+                srtd.add(nums1[a]);
+                a++;
+            } else if (nums1[a] > nums2[b]) {
+                srtd.add(nums2[b]);
+                b++;
+            } else {
+                srtd.add(nums1[a]);
+                srtd.add(nums2[b]);
+                a++;
+                b++;
             }
         }
-
-        System.out.println(Arrays.toString(medianArr));
-        
-        if (!(medianArr.length%2==0)) return medianArr[medianArr.length/2];
-        
-        int sum = medianArr[medianArr.length/2] + medianArr[(medianArr.length/2)-1];
-        return (double)sum / 2;
+        while (a < l1) {
+            srtd.add(nums1[a]);
+            a++;
+        }
+        while (b < l2) {
+            srtd.add(nums2[b]);
+            b++;
+        }
+        double median = 0;
+        int end = srtd.size() - 1;
+        if (end % 2 != 0) {
+            median = (srtd.get(end / 2) + srtd.get((end / 2) + 1)) / (double) 2;
+        } else {
+            median = (double) srtd.get(end / 2);
+        }
+        return median;
     }
 }
