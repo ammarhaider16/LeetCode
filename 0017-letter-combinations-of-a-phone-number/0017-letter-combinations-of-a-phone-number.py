045bfa4f265ad3dc84bd6b71e17de4cd4f3dd6a0
Ammar Haider <148816794+ammarhaider16@users.noreply.github.com>
2024-10-16 21:32:51 -0400

    Time: 22 ms (7.24%), Space: 11.7 MB (60.49%) - LeetHub

---
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
@@ -0,0 +1,38 @@
+class Solution(object):
+
+    def letterCombinations(self, digits):
+        """
+        :type digits: str
+        :rtype: List[str]
+        """
+
+        if (len(digits)==0):
+            return []
+
+        self.digitsList = list(digits)
+        self.combinations = []
+        prefix = ""
+        self.helper(prefix, 0)
+        return self.combinations
+
+
+    def helper(self,prefix,idx):
+        if (idx == len(self.digitsList)):
+            self.combinations.append(prefix)
+            return
+        nextChars = self.letters[self.digitsList[idx]]
+        for char in nextChars:
+            self.helper(prefix+char, idx+1)
+        return
+        
+
+    letters = {
+        "2":["a","b","c"],
+        "3":["d","e","f"],
+        "4":["g","h","i"],
+        "5":["j","k","l"],
+        "6":["m","n","o"],
+        "7":["p","q","r","s"],
+        "8":["t","u","v"],
+        "9":["w","x","y","z"]
+    }
