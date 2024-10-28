3cc338e9820e23ac29e3addab632fdc87bfcd9a5
Ammar Haider <148816794+ammarhaider16@users.noreply.github.com>
2024-10-28 18:43:38 -0400

    Time: 78 ms (18.79%), Space: 20.5 MB (94.55%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.py
+++ 0053-maximum-subarray/0053-maximum-subarray.py
@@ -5,15 +5,12 @@ class Solution(object):
         :rtype: int
         """
         n = len(nums)
-        dp = [0]*len(nums)
-        dp[n-1] = nums[n-1]
-        globalMax = dp[n-1]
+        globalMax = nums[n-1]
 
         for i in range(n-2,-1,-1):
-            dp[i] = max(nums[i], nums[i]+dp[i+1])
-            globalMax = max(globalMax, dp[i])
+            compare = nums[i]+nums[i+1]
+            if (nums[i] < compare):
+                nums[i] = compare
+            globalMax = max(globalMax, nums[i])
 
         return globalMax
-
-
-
