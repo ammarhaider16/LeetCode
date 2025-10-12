db272f043922791823a013bbfedb19ba7d0320aa
Ammar Haider <148816794+ammarhaider16@users.noreply.github.com>
2025-10-11 21:29:12 -0400

    Time: 35 ms (21.97%), Space: 19.6 MB (11.09%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.py
+++ 0695-max-area-of-island/0695-max-area-of-island.py
@@ -1,12 +1,6 @@
 class Solution:
     def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
         n, m = len(grid), len(grid[0])
-        starting_points = []
-
-        for i in range(n):
-            for j in range(m):
-                if grid[i][j] == 1:
-                    starting_points.append((i, j))
         visited = set()
         max_area = 0
         directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
@@ -23,9 +17,9 @@ class Solution:
                     area += area_dfs(neighbor)
             return area
 
-        for point in starting_points:
-            if point not in visited:
-                area_from_point = area_dfs(point)
+        for i in range(n):
+            for j in range(m):
+                area_from_point = area_dfs((i, j)) if grid[i][j] == 1 else 0
                 if area_from_point > max_area: 
                     max_area = area_from_point
         return max_area
