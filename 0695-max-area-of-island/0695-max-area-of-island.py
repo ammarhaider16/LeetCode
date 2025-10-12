21c0f9f315a93b8e12f2df632d3738146fde5e33
Ammar Haider <148816794+ammarhaider16@users.noreply.github.com>
2025-10-11 21:23:27 -0400

    Time: 37 ms (16.39%), Space: 19.7 MB (11.09%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.py
+++ 0695-max-area-of-island/0695-max-area-of-island.py
@@ -3,7 +3,6 @@ class Solution:
         n, m = len(grid), len(grid[0])
         starting_points = []
 
-        # Populate starting points
         for i in range(n):
             for j in range(m):
                 if grid[i][j] == 1:
@@ -17,7 +16,7 @@ class Solution:
                 return 0
             visited.add(point)
             area = 1 
-            # chance of mistake in area calculation if you visualize the dfs incorrectly -- this captures edge cases of no more neigbors, prevents double counting, also safe because fun called on this point only when point is 1 and the 1 in this point has not contributed to the area
+
             for direction in directions:
                 neighbor = (point[0] + direction[0], point[1] + direction[1])
                 if self.validate_index(neighbor, n, m) and grid[neighbor[0]][neighbor[1]] == 1:
@@ -31,6 +30,5 @@ class Solution:
                     max_area = area_from_point
         return max_area
 
-    # Need index validation for DFS
     def validate_index(self, point, n, m):
         return True if (point[0] >= 0 and point[0] < n and point[1] >= 0 and point[1] < m) else False
