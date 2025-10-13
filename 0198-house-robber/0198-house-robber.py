b333e8fc4c88039865d644e7e36c1d90670630a6
Ammar Haider <148816794+ammarhaider16@users.noreply.github.com>
2025-10-13 15:15:03 -0400

    Time: 0 ms (100%), Space: 17.9 MB (13.13%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -8,15 +8,12 @@ class Solution:
         dp = [0] * n
         dp[n-1] = nums[n-1]
         dp[n-2] = nums[n-2]
-        max_rob = max(nums[n-1], nums[n-2]) 
         for i in range(n-3, -1, -1): 
             max_after = 0
             for j in range(i+2, n):
                 if dp[j] > max_after:
                     max_after = dp[j]
                 dp[i] = nums[i] + max_after 
-                if dp[i] > max_rob:
-                    max_rob = dp[i]	
-        return max_rob
+        return max(dp)
 
     
