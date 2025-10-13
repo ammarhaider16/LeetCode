6a9316e32d015b11dd3141c2252a6da81208bea9
Ammar Haider <148816794+ammarhaider16@users.noreply.github.com>
2025-10-13 02:38:51 -0400

    Time: 187 ms (17.61%), Space: 59.7 MB (55.44%) - LeetHub

--- 0077-combinations/0077-combinations.py
+++ 0077-combinations/0077-combinations.py
@@ -3,14 +3,14 @@ class Solution:
         if n<k:
             return []
         all_combinations = []
-        def make_combination(n, last_selected, remaining, combination):
+        def make_combination(last_selected, remaining, combination):
             if remaining == 0:
                 all_combinations.append(combination[:])
             if last_selected == n:
                 return 
             for i in range(last_selected+1, n+1):
                 combination.append(i)
-                make_combination(n, i, remaining - 1, combination)
+                make_combination(i, remaining - 1, combination)
                 combination.pop()
-        make_combination(n, 0, k, [])
+        make_combination(0, k, [])
         return all_combinations
