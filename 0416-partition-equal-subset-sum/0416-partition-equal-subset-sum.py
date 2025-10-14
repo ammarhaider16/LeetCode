19c901e7a9df4faf38b324436c4ed5cb93d84f5e
Ammar Haider <148816794+ammarhaider16@users.noreply.github.com>
2025-10-13 21:37:13 -0400

    Time: 2225 ms (30.97%), Space: 28.4 MB (33.77%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
@@ -4,15 +4,11 @@ class Solution:
         if n_2 % 2 != 0:
             return False
         m, n = len(nums), n_2 // 2 
-        dp = [[False]*(n+1) for _ in range(m)]
-        for i in range(m):
-            dp[i][0] = True
-        for j in range(1, n+1):
-            dp[0][j] = True if nums[0] == j else False
+        dp = [[False]*n for _ in range(m)]
+        for j in range(n):
+            dp[0][j] = True if nums[0] == (j+1) else False
         for i in range(1, m):
-            for j in range(1, n+1):
-                use_index = j - nums[i]
-                dp[i][j] = True if (use_index >= 0 and dp[i-1][use_index]) or (dp[i-1][j]) else False
-        return dp[m-1][n]
-
-    
+            for j in range(n):
+                use_index = (j - nums[i])
+                dp[i][j] = True if (use_index >= 0 and dp[i-1][use_index]) or (dp[i-1][j]) or (use_index == -1) else False
+        return dp[m-1][n-1]   # Correct, now optimizing
