bab72a93bab2fe043ad3a2cc6bee45d67bb27f01
Ammar Haider <148816794+ammarhaider16@users.noreply.github.com>
2025-10-20 19:44:43 -0400

    Time: 5 ms (34.64%), Space: 20.3 MB (62.8%) - LeetHub

--- 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.py
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.py
@@ -1,14 +1,14 @@
 class Solution:
     def kthSmallest(self, root, k):
-        res = [-1] 
+        self.res = -1 
         def search(current, parent_smaller):
             if not current:
                 return 0
             left_size = search(current.left, parent_smaller)
             curr_smaller = left_size + parent_smaller
             if curr_smaller == (k - 1):
-                res[0] = current.val
+                self.res = current.val
             right_size = search(current.right, curr_smaller + 1)
             return left_size + 1 + right_size
         search(root, 0)
-        return res[0]
+        return self.res
