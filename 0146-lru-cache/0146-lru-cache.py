0a23379ca09d4640269ef29c8d4b682fa33095f1
Ammar Haider <148816794+ammarhaider16@users.noreply.github.com>
2025-10-20 03:23:05 -0400

    Time: 2879 ms (5.01%), Space: 78.3 MB (22.74%) - LeetHub

---
+++ 0146-lru-cache/0146-lru-cache.py
@@ -0,0 +1,27 @@
+class LRUCache:
+    def __init__(self, capacity: int):
+        self.current_action_number = 0
+        self.capacity = capacity
+        self.cache = {} # key -> [last_used, value]
+       
+    def get(self, key: int) -> int:
+        if key not in self.cache:
+            return -1
+        self.current_action_number += 1
+        self.cache[key][1] = self.current_action_number
+        return self.cache[key][0]
+
+    def put(self, key: int, value: int) -> None:
+        self.current_action_number += 1
+        if key in self.cache:
+            self.cache[key] = [value, self.current_action_number]  
+            return
+        
+        if len(self.cache) == self.capacity:
+            min_key, min_value = None, float('inf')
+            for k, v in self.cache.items():
+                if v[1] < min_value:
+                    min_value = v[1]
+                    min_key = k
+            self.cache.pop(min_key)
+        self.cache[key] = [value, self.current_action_number]  
