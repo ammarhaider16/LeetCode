b03a72cc57618494fafb5efb30429000b320194e
Ammar Haider <148816794+ammarhaider16@users.noreply.github.com>
2025-10-21 00:56:46 -0400

    Time: 662 ms (24.52%), Space: 18.5 MB (91.99%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.py
+++ 0045-jump-game-ii/0045-jump-game-ii.py
@@ -5,10 +5,10 @@ class Solution:
         dp[n-1] = 0
         for i in range(n-2, -1, -1):
             min_after = float('inf')
-            max_jump_size = min(n-1-i, nums[i]) + 1  # prevent over indexing
-            for jump in range(1, max_jump_size): 
-                if dp[i+jump] < min_after:
-                    min_after = dp[i+jump]
+            upper_bound = min(n, i + 1 + nums[i])
+            for jump in range(i+1, upper_bound): 
+                if dp[jump] < min_after:
+                    min_after = dp[jump]
             if min_after < float('inf'):
                 dp[i] = 1 + min_after
         return dp[0]
