d709fc535f12100ff8a936a403618060c89654e9
Ammar Haider <148816794+ammarhaider16@users.noreply.github.com>
2026-09-14 15:09:33 -0400

    Time: 276 ms (84.63%), Space: 0B (100%) - LeetHub

--- 0176-second-highest-salary/0176-second-highest-salary.sql
+++ 0176-second-highest-salary/0176-second-highest-salary.sql
@@ -1,11 +1,7 @@
 # Write your MySQL query statement below
-WITH ranked_salaries AS (
-    SELECT salary, DENSE_RANK() OVER(ORDER BY salary DESC) as salary_rank
-    FROM Employee 
-    UNION
-    SELECT NULL AS salary, 2 AS salary_rank
-)
-SELECT salary AS SecondHighestSalary
-FROM ranked_salaries
-WHERE salary_rank = 2
-LIMIT 1
+SELECT (
+    SELECT DISTINCT salary
+    FROM Employee
+    ORDER BY salary DESC
+    LIMIT 1 OFFSET 1
+) AS SecondHighestSalary
