2a4249d806faaae9ff7b3765737d5a647e57bff2
Ammar Haider <148816794+ammarhaider16@users.noreply.github.com>
2026-09-14 15:18:30 -0400

    Time: 625 ms (95.26%), Space: 0B (100%) - LeetHub

--- 1934-confirmation-rate/1934-confirmation-rate.sql
+++ 1934-confirmation-rate/1934-confirmation-rate.sql
@@ -1,17 +1,7 @@
 # Write your MySQL query statement below
 
-# first need to get all the actions / user
-
-WITH confirmation_stats AS (
-    SELECT user_id, time_stamp, action, 
-        CASE
-            WHEN action = 'timeout' THEN 0
-            WHEN action = 'confirmed' THEN 1
-        END AS action_value
-FROM Confirmations
-)
-SELECT s.user_id, ROUND(COALESCE(SUM(cs.action_value)/COUNT(cs.action_value), 0),2) AS confirmation_rate
-FROM Signups s LEFT JOIN confirmation_stats cs ON s.user_id = cs.user_id
-GROUP BY s.user_id;
-
+SELECT S.user_id, 
+    ROUND(COALESCE(SUM(CASE WHEN C.action = 'confirmed' THEN 1 ELSE 0 END) / COUNT(*), 0), 2) AS confirmation_rate
+FROM Signups S LEFT JOIN Confirmations C ON S.user_id = C.user_id
+GROUP BY S.user_id
 
