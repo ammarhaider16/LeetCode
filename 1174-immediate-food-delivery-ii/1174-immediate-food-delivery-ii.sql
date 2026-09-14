1aebe1cf5bc47224c4b00fb0016ced77e7b05691
Ammar Haider <148816794+ammarhaider16@users.noreply.github.com>
2026-09-13 23:46:57 -0400

    Time: 684 ms (63.2%), Space: 0B (100%) - LeetHub

--- 1174-immediate-food-delivery-ii/1174-immediate-food-delivery-ii.sql
+++ 1174-immediate-food-delivery-ii/1174-immediate-food-delivery-ii.sql
@@ -1,13 +1,14 @@
 # Write your MySQL query statement below
 
 
-WITH immediate_customers AS (
-    SELECT 
-    A.delivery_id, 
-    A.customer_id, 
-    (CASE WHEN DATEDIFF(A.order_date, A.customer_pref_delivery_date) = 0 THEN 1 ELSE 0 END) AS is_immediate
-FROM Delivery A
-WHERE A.order_date = (SELECT MIN(B.order_date) as order_date FROM Delivery B WHERE A.customer_id = B.customer_id)
+WITH ranked_customers AS (
+    SELECT
+        customer_id,
+        CASE WHEN DATEDIFF(order_date, customer_pref_delivery_date) = 0 THEN 1 ELSE 0 END AS is_immediate,
+        ROW_NUMBER() OVER(PARTITION BY customer_id ORDER BY order_date) as order_number   
+    FROM Delivery
 )
-SELECT 100*ROUND(SUM(is_immediate) / COUNT(*), 4) as immediate_percentage
-FROM immediate_customers
+SELECT 100*ROUND(SUM(is_immediate)/COUNT(*), 4) as immediate_percentage
+FROM ranked_customers
+WHERE order_number = 1;
+
